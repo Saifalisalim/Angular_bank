c1dd287b6855011f576b964b19e1b1c1fdaf177f
Saif Ali Salim <saifalisalim@gmail.com>
2023-05-08 11:57:56 +0530

    1st commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  data="Your Perfect Banking Partner"
+  placeHolderData="Enter Account Number"
+
+  constructor() { }
+
+  ngOnInit(): void{
+
+  }
+
+  login(){
+    alert("login clicked")
+  }
+
+  unameChange(event:any){
+    console.log(event.target.value);
+    
+  }
+
+}
